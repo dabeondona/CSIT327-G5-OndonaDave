e5ee22f6a3854203ea69c24e006b62383bf16c12
Dave Ondona <ondonadave5@gmail.com>
2023-12-12 22:46:33 +0800

    Made Initial Additions as well as Views

--- app.py
+++ app.py
@@ -1,64 +1,164 @@
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+from sqlalchemy.sql import text
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 
-class Todo(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(200), nullable=False)
-    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+class Furniture(db.Model):
+    furniture_id = db.Column(db.Integer, primary_key=True)
+    furniture_name = db.Column(db.String(200), nullable=False)
+    furniture_type = db.Column(db.String(100))
+    furniture_weight = db.Column(db.Float)
+    furniture_price = db.Column(db.Float)
 
     def __repr__(self):
-        return '<Task %r>' % self.id
+        return '<Furniture %r' % self.id
+    
+class Shoes(db.Model):
+    shoes_id = db.Column(db.Integer, primary_key=True)
+    shoes_name = db.Column(db.String(200), nullable=False)
+    shoes_brand = db.Column(db.String(100))
+    shoes_size = db.Column(db.Integer)
+    shoes_gender = db.Column(db.String(50))
+    shoes_color = db.Column(db.String(50))
+    shoes_price = db.Column(db.Float)
+
+    def __repr__(self):
+        return '<Shoes %r>' % self.id
+    
+class Appliances(db.Model):
+    appliance_id = db.Column(db.Integer, primary_key=True)
+    appliance_name = db.Column(db.String(200), nullable=False)
+    appliance_type = db.Column(db.String(100))
+    appliance_brand = db.Column(db.String(100))
+    appliance_weight = db.Column(db.Float)
+    appliance_voltage = db.Column(db.Integer)
+    appliance_price = db.Column(db.Float)
+    
+    def __repr__(self):
+        return '<Appliances %r>' % self.id
+
+class Stationery(db.Model):
+    stationery_id = db.Column(db.Integer, primary_key=True)
+    stationery_name = db.Column(db.String(200), nullable=False)
+    stationery_brand = db.Column(db.String(100))
+    stationery_type = db.Column(db.String(100))
+    stationery_quantity = db.Column(db.Integer)
+    stationery_price = db.Column(db.Float)
+
+    def __repr__(self):
+        return '<Stationery %r>' % self.id
+
+def create_view():
+    with app.app_context():
+        view_query = text("""
+        CREATE VIEW IF NOT EXISTS category_summary AS 
+        SELECT 'Furniture' AS category, COUNT(*) AS count FROM furniture
+        UNION ALL
+        SELECT 'Shoes', COUNT(*) FROM shoes
+        UNION ALL
+        SELECT 'Appliances', COUNT(*) FROM appliances
+        UNION ALL
+        SELECT 'Stationery', COUNT(*) FROM stationery;
+        """)
+        db.session.execute(view_query)
+        db.session.commit()
 
 
-@app.route('/', methods=['POST', 'GET'])
+    
+@app.route('/')
 def index():
+    result = db.session.execute(text('SELECT * FROM category_summary'))
+    categories = [{'category': row[0], 'count': row[1]} for row in result]
+    return render_template('index.html', categories=categories)
+    
+@app.route('/furniture', methods=['POST', 'GET'])
+def furniture():
     if request.method == 'POST':
-        task_content = request.form['content']
-        new_task = Todo(content=task_content)
-
-        try:
-            db.session.add(new_task)
-            db.session.commit()
-            return redirect('/')
-        except:
-            return 'There was an issue adding your task'
-
+        furniture_name = request.form.get('furniture_name')
+        furniture_type = request.form.get('furniture_type')
+        furniture_weight = request.form.get('furniture_weight')
+        furniture_price = request.form.get('furniture_price')
+        
+        if furniture_name and furniture_type and furniture_weight and furniture_price:
+            try:
+                new_furniture = Furniture(
+                    furniture_name=furniture_name,
+                    furniture_type=furniture_type,
+                    furniture_weight=float(furniture_weight),
+                    furniture_price=float(furniture_price)
+                )
+                db.session.add(new_furniture)
+                db.session.commit()
+                return redirect('/furniture')
+            except Exception as e:
+                return f'An error occurred when adding the furniture: {e}'
+    
+    furniture_items = Furniture.query.all()
+    return render_template('furniture.html', furniture_items=furniture_items)
+    
+@app.route('/shoes')
+def shoes():
+        return render_template('shoes.html')
+    
+@app.route('/appliances')
+def appliances():
+        return render_template('appliances.html')
+    
+@app.route('/stationery')
+def stationery():
+        return render_template('stationery.html')
+    
+def get_model_by_category(category):
+    if category == 'furniture':
+        return Furniture
+    elif category == 'shoes':
+        return Shoes
+    elif category == 'appliances':
+        return Appliances
+    elif category == 'stationery':
+        return Stationery
     else:
-        tasks = Todo.query.order_by(Todo.date_created).all()
-        return render_template('index.html', tasks=tasks)
+        return None
 
-@app.route('/delete/<int:id>')
-def delete(id):
-    task_to_delete = Todo.query.get_or_404(id)
+@app.route('/delete/<category>/<int:id>')
+def delete(category, id):
+    model = get_model_by_category(category)
+    if model is None:
+        return 'Invalid category'
+
+    item_to_delete = model.query.get_or_404(id)
     
     try:
-        db.session.delete(task_to_delete)
+        db.session.delete(item_to_delete)
         db.session.commit()
         return redirect('/')
     except:
-        return 'There was a problem deleting that task'
-  
-@app.route('/update/<int:id>', methods=['GET', 'POST'])
-def update(id):
-    task = Todo.query.get_or_404(id)
+        return 'There was a problem deleting that item'
+    
+@app.route('/update/furniture/<int:id>', methods=['GET', 'POST'])
+def furnitureUpdate(id):
+    furniture_item = Furniture.query.get_or_404(id)
 
     if request.method == 'POST':
-        task.content = request.form['content']
+        furniture_item.furniture_name = request.form.get('furniture_name')
+        furniture_item.furniture_type = request.form.get('furniture_type')
+        furniture_item.furniture_weight = float(request.form.get('furniture_weight', 0))
+        furniture_item.furniture_price = float(request.form.get('furniture_price', 0))
 
         try:
             db.session.commit()
-            return redirect('/')
-        except:
-            return 'There was an issue updating your task'
+            return redirect('/furniture')
+        except Exception as e:
+            return f'There was an issue updating the furniture item: {e}'
 
     else:
-        return render_template('update.html', task=task)
-
+        return render_template('furnitureupdate.html', item=furniture_item)
     
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all() 
+        create_view()    
     app.run(debug=True)
